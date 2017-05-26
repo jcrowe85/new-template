1ad12eaae38475c1dd9728fed1c13b562a6ed876
Josh <jcrowe120485@gmail.com>
2017-05-26 18:39:33 +0000

    edit life.php fieldsets to not animate #40

--- slide-next.php
+++ slide-next.php
@@ -20,9 +20,9 @@ var animating; //flag to prevent quick multi-click glitches
 		step: function(now, mx) {
 			//as the opacity of current_fs reduces to 0 - stored in "now"
 			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
+			<!--scale = 1 - (1 - now) * 0.2;-->
 			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
+			<!--left = (now * 50)+"%";-->
 			//3. increase opacity of next_fs to 1 as it moves in
 			opacity = 1 - now;
 			current_fs.css({'transform': 'scale('+scale+')'});
