e3c0fcf5fd9f8b2525d8b9ae2b7b37a8029bb0ca
Josh <jcrowe120485@gmail.com>
2017-05-31 21:19:26 +0000

    add mouseflow

--- mouseflow.php
+++ mouseflow.php
@@ -1,10 +1,9 @@
 <script type="text/javascript">
-// jcrowe@sunpathfinancial.com - paid account 2.5k @ 29.99/mo
-    var _mfq = _mfq || [];
+    window._mfq = window._mfq || [];
     (function() {
         var mf = document.createElement("script");
         mf.type = "text/javascript"; mf.async = true;
-        mf.src = "//cdn.mouseflow.com/projects/b7dd2d07-5f4d-4c76-b57f-1c2ea24d1ae3.js";
+        mf.src = "//cdn.mouseflow.com/projects/edd0d33c-2ba8-4bcb-ba3f-e5b6e4bd2374.js";
         document.getElementsByTagName("head")[0].appendChild(mf);
     })();
 </script>
