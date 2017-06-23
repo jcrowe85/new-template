d962fa945b87b56524cd4c880021659c59affbdd
Josh <jcrowe120485@gmail.com>
2017-06-23 00:14:16 +0000

    stylize #159

--- head.php
+++ head.php
@@ -72,5 +72,18 @@
         <meta property="og:title" content="Usurit"/>
         <meta property="og:image" content="/images/usurit-facelift.png"/>
         <meta property="og:description" content="Making Insurance Easy to Learn, and Easy to Buy."/>
-        <meta property="og:url" content="www.usurit.com/" />        
+        <meta property="og:url" content="www.usurit.com/" />     
+        
+        <!-- Begin Inspectlet Embed Code -->
+        <script type="text/javascript" id="inspectletjs">
+        window.__insp = window.__insp || [];
+        __insp.push(['wid', 256262892]);
+        (function() {
+        function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+        setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
+        })();
+        </script>
+        <!-- End Inspectlet Embed Code -->    
+        
+        
     </head>    
