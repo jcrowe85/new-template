2540131c42925043e6fd6478f10e78eb7abf6d59
Josh <jcrowe120485@gmail.com>
2017-08-18 04:50:04 +0000

    add life insurance overview and when to buy life insurance to new template #244

---
+++ life-insurance-overview2.php
@@ -0,0 +1,69 @@
+<?php include_once('head.php') ?>   
+<?php include_once('privy.php') ?>  
+<style type="text/css">
+    ol{
+        padding-left: 35px;
+    }
+</style>
+    <body id="lifeguide-page">
+        <?php include_once('header.php') ?>
+            <div class="container">
+                <div class="row guide">
+                    <div class="title">
+                        <h1>What is life insurance?</h1>
+                    </div>
+                    <div class="about">
+
+
+<p>To put it simply, it's an insurance policy that pays out a lump sum of cash, tax-free, to the named beneficiary upon the policyholder's death.</p>
+ 
+<h4>Two Types</h4>
+<p>There are generally two types of insurance policies. Term life insurance and Permanent Life insurance. We'll break down the easier one first.</p>
+ 
+<h4>Term Life Insurance</h4>
+<p>
+The most commonly bought life insurance type. It's the cheapest out there because it expires. It's not meant to gamble on when you expect to die, it's to protect your family in the unlikely case that you pass away unexpectedly.
+There are 4 types of term policies. Level-Term, Return of Premium, Renewable Term, and Decreasing-Term. Only two of these policies are really rare because of the lack of benefit they provide.
+</p>
+
+<h4>Level-Term</h4>
+<p>It's the simplest to understand. It's usually in bands of 10, 15, 20, 25, and 30 years, after the term period is done, it expires. The policy also has level premiums throughout the term period, simply meaning that the prices do not change at all for the entire duration of the policy. Out of all the term life insurance policies, this is the most popular.</p>
+ 
+<h4>Return of Premium</h4>
+<p>As the name implies, it functions similarly to level-term policies. But the added benefit is that you actually receive ALL of the premiums that you paid in when the policy expires, and it's given back all at once in one big check. Premiums do not change at all during the entire duration of the policy.</p>
+ 
+<h4>Renewable Term</h4>
+<p>It works as a 5 year term policy (or whatever the renewable term length is, usually 5), and at the end of the term, the policy renews itself, and it can continue to renew itself to a certain maximum age (usually around 80). What's different about this policy is that every 5 years, the premiums change. So at first, the cost to buy this policy when someone is young will be really cheap, but as they get older, it becomes astronomically higher. Generally speaking, it's cheaper in the long run to buy a level-term policy if you plan to hold it for more than 20 years.</p>
+ 
+<h4>Decreasing Level Term</h4>
+<p>A very rare term policy and almost all carriers no longer sell this product. It acts similarly to the level-term policy in that the premiums can never change. The biggest thing is that as every year passes, the death benefit decreases and they're structured that by the end of the term length, the insurance policy would have no more value. The reason why they are so rare is because level term has become so competitive, that for a few dollars more every year your beneficiaries would be able to have a much larger benefit.</p>
+ 
+<h4>Permanent Life Insurance</h4>
+<p>It's what most people think about when they think life insurance. It's guaranteed to payout as long as you pay the premiums, the downside is that these policies can have you pay for a long period of time or even until you pass away.</p>
+ 
+<h4>Whole Life</h4>
+<p>The oldest type of life insurance policy, it's probably what your parents had and their parents had as well. It is the most expensive life insurance policy in the market. Simply because it's not only a life insurance policy, but a forced savings account as well. Whole life policies give out dividends, and these dividends increase as you put in more money into the policy. Generally speaking, these dividends can range anywhere between 1-4%.
+    And the selling point is that you can actually take out these dividends in the future, you can use them to pay off future premiums, in effect making the policy “paid up”, you can withdraw those dividends for your own use, or you can take a loan out against the cash value of those dividends. 
+    However, these whole life policies have huge stipulations in terms of getting the dividend, and in some cases, it could be 25-30 years before you have access to the cash.</p>
+ 
+<h4>Universal Life</h4>
+<p>Was created in a response to how expensive Whole Life policies were. It's the permanent life insurance policy without any bells or whistles. That means it's the cheapest type of permanent policy in the market.</p>
+ 
+<h4>Variable Life</h4>
+<p>Was an effort to try to further reduce the monthly cost of insurance as well as playing the stock market. These policies are largely extinct because of the uncertainty these policies created. Insurance is supposed to be stable, these policies were the opposite of stability.
+    The consumer would pay a portion of the premium and the other portion would come from the returns of the stock market. Then sometime in the future, the insurance carrier would add up how much money the consumer put in and how well the stock market did, and if that was less than a certain amount. They would charge you the remaining balance and add it ON TOP of the monthly premium. However, if the market returns did well, then the premiums could actually be reduced, because of the performance of the market.
+    </p>
+ 
+<h4>Variable Universal Life</h4>
+<p>This is what was supposed to replace variable life policies. Instead of forcing people an amount to pay for the forced savings portion (dividends), they allowed for people to put in as much money as they wanted. As long as the base premium was paid off (the universal life part), they could dump in as much or as little as they wanted. And instead of receiving a small interest rate, that money would be invested into the stock market.</p>
+
+
+
+ 
+
+                    </div>
+                </div>
+            </div>
+            <?php include_once('_getstarted.php'); ?>
+<?php include_once('footer.php') ?>
+
--- term/term-life-insurance-guide.php
+++ term/term-life-insurance-guide.php
@@ -1,80 +1,261 @@
 <?php include_once('../head.php') ?>   
 <?php include_once('../privy.php') ?>  
 <style type="text/css">
+    body{
+         color:#4d4d4d;
+    }
     ol{
         padding-left: 35px;
     }
+    #lifeguide-page .mod-row {
+        margin:0 auto;
+        margin-top:100px;
+        width:70%;
+    }
+    #lifeguide-page h3{
+        margin-bottom:50px;
+        position: relative;
+        font-weight: 600;
+        
+        display: inline-block;
+    }
+    .underline:after{
+    content: '';
+    position: absolute;
+    bottom: -2rem;
+    left: 50%;
+    width: calc(100% + 1.0rem);
+    border-bottom: 1px solid #e6e6e6;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+    } 
+    .riders p{
+        margin-bottom:5rem;
+    }
+    .navbar a:link{
+        color:black;
+    }
+    .navbar a:active{
+        color:black;
+    }  
+    .navbar a:visited{
+        color:black;
+    }      
+    .logo span{
+        color:black;
+    }
+    .header{
+        background: white;
+    }
+    .navbar-toggle{
+        border: 1px solid #fff;
+    }
+    .navbar-toggle span{
+        background: #fff;
+    }
+    .pros{
+        margin-bottom:25px;
+        display: -webkit-box;
+        display: -moz-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        align-items: center;
+    }
+    .cons{
+        margin-bottom:25px;
+        display: -webkit-box;
+        display: -moz-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        align-items: center;
+    }
+    .title{
+        margin-top:50px;
+        text-align:center; 
+        padding: 80px 0px;
+    }
+    .title h1{
+        text-shadow: 1px 1px 1px rgba(0,0,0, 0.5); 
+        margin-bottom:25px;
+    }
+    @media screen and (max-width:768px){
+        #lifeguide-page .mod-row {
+            margin:0 auto;
+            margin-top:100px;
+            width:90%;
+        }
+    }
 </style>
+<script>
+    $(document).ready(function(){
+      $(".logo img").attr("src", "/images/logo-red.png");       
+    });
+</script>
     <body id="lifeguide-page">
         <?php include_once('../header.php') ?>
-            <div class="container">
-                <div class="row guide">
-                    <div class="title">
-                        <h1>When to Buy Term Life Insurance</h1>
+        <div class="container-fluid"  id="title-bg" style="text-align:center;">        
+            <div class="title">
+                <h1>When to Buy Term Life Insurance</h1>  
+                <!--<h4 style="font-family:'AileronThin'; text-shadow: 1px 1px 1px rgba(0,0,0, 0.5);">This is a short description of ROP This is a short description of ROP This is a short description of ROP</h4>-->
+                <form action="/life.php">
+                    <button class="btn btn-primary select btn-lg" style="padding: 10px 50px; background:#73a5fb; border:none; box-shadow: 0px 5px 1px rgba(0,0,0,0.5); margin-bottom:10px;">GET QUOTES</button>   
+                </form>
+                <div><a href="tel:+9494384333" style="text-decoration:underline; color:white; font-family:'AileronThin'; font-size:15px;"><i class="fa fa-phone" aria-hidden="true"></i> Speak with an agent</a></div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row mod-row">
+                    <div style="text-align:center;">
+                        <h3 class="underline">When Should You Buy Term Life Insurance?</h3>
                     </div>
-                    <div class="about">
-<p>Many people think that term is the only type of insurance to buy. And for a lot of people, that would make sense. Generally speaking, term covers a lot more common necessities and reasons for why people look for life insurance.</p>
- 
-<p>So what are these common necessities? Most of them are just temporary needs, and while some things feel like they would be long-term, a lot of the problems could be solved with term insurance.</p>
- 
-<h4>To clarify, these are the reasons for term insurance:</h4>
-
-<ol>
-    <li><p>Any debt obligations</p></li>
-    <li><p>Making sure that children are taken care of as well as college expenses</p></li>
-    <li><p>Spouse or loved one is supported while they get back on their feet</p></li>
-    <li><p>Temporary need of Burial expenses</p></li>
-    <li><p>Paying off any estate taxes</p></li>
-    <li><p>Thinking your health situation may change</p></li>
-</ol>
- 
-<h4>It should not be a solution to these:</h4>
-<ol>
-    <li>
-        <p>Getting your family rich</p>
-    </li>
-    <li>
-        <p>Inheritance for your loved ones</p>
-    </li>
-    <li>
-        <p>Social Security or pension supplement/replacement</p>
-    </li>
-    <li>
-        <p>As an investment savings platform or a savings plan</p>
-    </li>
-</ol>
- 
-<h4>Most popular things to cover with Term Life Insurance</h4>
-
-<ol>
-    <li><p>Personal loans, student loans, and credit card debt</p></li>
-    <li>
-        <p>Mortgages</p>
-    </li>
-    <li>
-        <p>Living expenses for 3-5 years</p>
-    </li>
-    <li>
-        <p>College tuition for public or private schools</p>
-    </li>
-    <li>
-        <p>Final Expenses</p>
-    </li>
-    <li>
-        <p>Estate taxes</p>
-    </li>
-    <li>
-        <p>Buy-Sell Agreements (If you own a business) </p>
-    </li>
-    <li>
-        <p>Key-Person Insurance</p>
-    </li>
-</ol>
- 
-
+                    <p>
+                    Many people think that term is the only type of insurance to buy. And for a lot of people, that would make sense. 
+                    Generally speaking, term covers a lot more common necessities and reasons for why people look for life insurance.
+                    </p>   
+                    <p>
+                    So what are these common necessities? Most of them are just temporary needs, and while some things feel like they would be long-term, 
+                    a lot of the problems could be solved with term insurance.                        
+                    </p>
+            </div>
+                <div class="row mod-row">
+                    <div class="col-md-6 col-md-offset-3" style="padding:0px;">
+                        <div style="text-align:center;">
+                            <h3 class="underline">Why Should You Consider It?</h3>
+                        </div>
+                        <div class="pros">
+                            <img src="/images/green-thumbs-up.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                            Any debt obligations
+                            </p>
+                            
+                        </div>
+                        <div class="pros">
+                            <img src="/images/green-thumbs-up.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                            Making sure that children are taken care of as well as college expenses
+                            </p>
+                        </div>   
+                        <div class="pros">
+                            <img src="/images/green-thumbs-up.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                             Spouse or loved one is supported while they get back on their feet
+                            </p>
+                        </div>             
+                        <div class="pros">
+                            <img src="/images/green-thumbs-up.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                            Temporary need of Burial expenses
+                            </p>
+                        </div> 
+                        <div class="pros">
+                            <img src="/images/green-thumbs-up.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                            Paying off any estate taxes
+                            </p>
+                        </div>  
+                        <div class="pros">
+                            <img src="/images/green-thumbs-up.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                            Thinking your health situation may change
+                            </p>
+                        </div>  
+                        
                     </div>
                 </div>
+                <div class="row mod-row">
+                    <div class="col-md-6 col-md-offset-3" style="padding:0px;">
+                        <div style="text-align:center;">
+                            <h3 class="underline">Why You Shouldn't Get It?</h3>
+                        </div>
+                        <div class="cons">
+                            <img src="/images/red-thumbs-down.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                               Getting your family rich
+                            </p>
+                        </div>
+                        <div class="cons">
+                            <img src="/images/red-thumbs-down.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                                Inheritance for your loved ones
+                            </p>
+                        </div>   
+                        <div class="cons">
+                            <img src="/images/red-thumbs-down.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                                Social Security or pension supplement/replacement
+                            </p>
+                        </div>   
+                        <div class="cons">
+                            <img src="/images/red-thumbs-down.png" style="width:10%;padding-right:10px;"></img>
+                            <p>
+                                As an investment savings platform or a savings plan
+                            </p>
+                        </div>                           
+                    </div>  
+                </div>
+                <div class="row mod-row">
+                    <div class="col-md-12" style="padding:0px;">
+                        <div style="text-align:center;">
+                            <h3 class="underline">Most Popular Things to Cover with Term Life Insurance</h3>
+                        </div>
+                        <div class="col-md-6">
+                        <div class="cons">
+                            
+                            <p>
+                               Personal loans, student loans, and credit card debt
+                            </p>
+                        </div>
+                        <div class="cons">
+                            
+                            <p>
+                                Mortgages
+                            </p>
+                        </div>   
+                        <div class="cons">
+                            
+                            <p>
+                                Living expenses for 3-5 years
+                            </p>
+                        </div>   
+                        <div class="cons">
+                            
+                            <p>
+                                College tuition for public or private schools
+                            </p>
+                        </div> 
+                        </div>
+                        <div class="col-md-6">
+                        <div class="cons">
+                            
+                            <p>
+                                Final Expenses
+                            </p>
+                        </div>  
+                        <div class="cons">
+                            
+                            <p>
+                                Estate taxes
+                            </p>
+                        </div>  
+                        <div class="cons">
+                            
+                            <p>
+                                Buy-Sell Agreements (If you own a business)
+                            </p>
+                        </div>  
+                        <div class="cons">
+                            
+                            <p>
+                                Key-Person Insurance
+                            </p>
+                        </div>  
+                        </div>
+                    </div>                    
+                </div>    
             </div>
-            <?php include_once('../_getstarted.php'); ?>
-<?php include_once('../footer.php') ?>
+        </div>
+    <?php include_once('../footer.php') ?>
 
